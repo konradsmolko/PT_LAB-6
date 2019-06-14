32e6dd4a009dec82b2776aac69e3a5d3c65c7089
student <student@nmkzllab>
2019-06-14 10:43:17 +0200

    Dodaj pliki projektów.

---
+++ lab6/App.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace lab6
+{
+    /// <summary>
+    /// Logika interakcji dla klasy App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static UdpClient udpClient = new UdpClient();
+
+        public static void Connect(string hostname, int port)
+        {
+            string text = "connect";
+            byte[] buf = Encoding.ASCII.GetBytes(text);
+            udpClient.Connect(hostname, port);
+            udpClient.Send(buf, buf.Length);
+        }
+
+        public static void Disconnect()
+        {
+            string text = "disconnect";
+            byte[] buf = Encoding.ASCII.GetBytes(text);
+            udpClient.Send(buf, buf.Length);
+            udpClient.Close();
+        }
+
+        public static void StartSending(Point start)
+        {
+            
+        }
+
+        public static void ContinueSending(Point point)
+        {
+
+        }
+
+        public static void StopSending()
+        {
+
+        }
+
+        public void Server(string ipaddr, int port)
+        {
+            Task.Factory.StartNew(
+                () =>
+                {
+                    IPAddress address = IPAddress.Parse(ipaddr);
+                    IPEndPoint endPoint = new IPEndPoint(address, port);
+                    udpClient.Receive(ref endPoint);
+                });
+        }
+    }
+}
---
+++ lab6/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab6
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        public void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Point start;
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                start = e.GetPosition(this);
+                App.StartSending(start);
+            }
+
+            
+        }
+
+        public void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            App.StopSending();
+        }
+
+        public void Canvas_MouseMove(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                var point = e.GetPosition(this);
+                App.ContinueSending(point);
+            }
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.Connect(ipaddr.Text, int.Parse(port.Text));
+
+            connectButton.IsEnabled = false;
+            disconnectButton.IsEnabled = true;
+        }
+
+        private void DisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            App.Disconnect();
+
+            connectButton.IsEnabled = true;
+            disconnectButton.IsEnabled = false;
+        }
+
+        private void HostButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StopHostingButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
