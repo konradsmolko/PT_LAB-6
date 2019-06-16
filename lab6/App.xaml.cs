66ac7855a66a11ae900d1364ac60bebb169a25a8
Konrad <17689319+konradsmolko@users.noreply.github.com>
2019-06-16 18:29:36 +0200

    working...

--- lab6/App.xaml.cs
+++ lab6/App.xaml.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Media;
 
 namespace lab6
 {
@@ -16,48 +21,157 @@ namespace lab6
     /// </summary>
     public partial class App : Application
     {
-        static UdpClient udpClient = new UdpClient();
+        private static Window thisWindow;
 
-        public static void Connect(string hostname, int port)
-        {
-            string text = "connect";
-            byte[] buf = Encoding.ASCII.GetBytes(text);
-            udpClient.Connect(hostname, port);
-            udpClient.Send(buf, buf.Length);
-        }
+        public static void InitializeApp(Window win) => thisWindow = win; // po co?
+    }
 
-        public static void Disconnect()
-        {
-            string text = "disconnect";
-            byte[] buf = Encoding.ASCII.GetBytes(text);
-            udpClient.Send(buf, buf.Length);
-            udpClient.Close();
-        }
+    public class Server
+    {
+        private readonly List<int> clientIDs = new List<int>();
+        private readonly ConcurrentQueue<Point> drawingQueue = new ConcurrentQueue<Point>();
+        private Canvas canvas;
+        private bool isRunning = false;
+        UdpClient listener;
+        IPEndPoint groupEP;
+        Task handler, drawer;
 
-        public static void StartSending(Point start)
+        public bool IsRunning { get => isRunning; }
+
+        public Server()
         {
             
         }
 
-        public static void ContinueSending(Point point)
+        public int Start(Canvas c, string hostname, int port)
         {
+            try
+            {
+                if (hostname == "localhost")
+                    //groupEP = new IPEndPoint(IPAddress.Any, port);
+                    groupEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+                else
+                    groupEP = new IPEndPoint(IPAddress.Parse(hostname), port);
+                
+                listener = new UdpClient(port);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Server failed to start!\nHostname: {hostname}\nPort: {port}");
+                Console.WriteLine(e.ToString());
+                return 1;
+            }
 
+            isRunning = true;
+            canvas = c;
+
+            handler = Task.Factory.StartNew(() => HandleClients());
+            drawer = Task.Factory.StartNew(() => DrawReceivedPoints());
+            Console.WriteLine($"Server started.\nAddress: {groupEP.ToString()}");
+            return 0;
         }
 
-        public static void StopSending()
+        private void HandleClients()
         {
-
-        }
-
-        public void Server(string ipaddr, int port)
-        {
-            Task.Factory.StartNew(
-                () =>
+            while (isRunning || listener.Available != 0) // handle clients until the server is stopped, and prevent any client from leaving their crap in the buffer
+            {
+                if (listener.Available == 0) continue; // this is here to prevent blocking (until a client gets processed) when we want to stop the server
+                Console.WriteLine("Handling client...");
+                byte[] data = listener.Receive(ref groupEP);
+                switch (Encoding.ASCII.GetString(data))
                 {
-                    IPAddress address = IPAddress.Parse(ipaddr);
-                    IPEndPoint endPoint = new IPEndPoint(address, port);
-                    udpClient.Receive(ref endPoint);
-                });
+                    case "connect": // respond with personalized listening port #
+                        break;
+                    case "disconnect": // remove client from list
+                        break;
+                    default: // handle point collection
+                        break;
+                }
+                // Console.WriteLine(Encoding.ASCII.GetString(data));
+                // todo
+            }
+        }
+
+        private void DrawReceivedPoints()
+        {
+            while (isRunning || !drawingQueue.IsEmpty)
+            {
+                
+                //canvas.Children.Add();
+            }
+        }
+
+        public int Stop()
+        {
+            isRunning = false;
+            Console.WriteLine("Server awaiting clients...");
+            handler.Wait(); // awaiting any clients being handled right now
+            drawer.Wait();
+            handler.Dispose();
+            drawer.Dispose();
+            listener.Close();
+
+            Console.WriteLine("Server stopped.");
+            return 0;
+        }
+    }
+
+    public class Client
+    {
+        private Guid guid = Guid.NewGuid(); // used for absolute unique client identification
+        private UdpClient client = new UdpClient();
+        IPEndPoint endpoint;
+
+        public Client()
+        {
+
+        }
+
+        public int Connect(string hostname, int port)
+        {
+            if (hostname == "localhost") hostname = "127.0.0.1";
+
+            try
+            {
+                endpoint = new IPEndPoint(IPAddress.Parse(hostname), port);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Client failed to connect!\nGUID: {guid}\n{hostname}");
+                Console.WriteLine(e.ToString());
+                return 1;
+            }
+            
+            
+            byte[] buf = Encoding.ASCII.GetBytes("connect");
+            client.Send(buf, buf.Length, endpoint);
+
+            Console.WriteLine($"Client connected.\nGUID: {guid}\nEndpoint: {endpoint.ToString()}");
+            return 0;
+        }
+
+        public void Disconnect()
+        {
+            byte[] buf = Encoding.ASCII.GetBytes("disconnect");
+            client.Send(buf, buf.Length, endpoint);
+            endpoint = null;
+
+            Console.WriteLine($"Client disconnecting.\nGUID: {guid}");
+        }
+
+        public void StartSending(Point start)
+        {
+
+        }
+
+        public void ContinueSending(Point point)
+        {
+
+        }
+
+        public void StopSending()
+        {
+
         }
     }
 }
--- lab6/MainWindow.xaml.cs
+++ lab6/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,63 +21,98 @@ namespace lab6
     /// </summary>
     public partial class MainWindow : Window
     {
+        Color currentColor = SystemColors.WindowFrameBrush.Color;
+        Point currentPoint;
+        Polyline currentPolyline;
+        Server server = new Server();
+        Client client = new Client();
+
         public MainWindow()
         {
             InitializeComponent();
-
-
+            App.InitializeApp(this);
+            canvas.IsEnabled = false;
         }
-
-        public void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            Point start;
-            if (e.ButtonState == MouseButtonState.Pressed)
-            {
-                start = e.GetPosition(this);
-                App.StartSending(start);
-            }
-
-            
-        }
-
-        public void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            App.StopSending();
-        }
-
-        public void Canvas_MouseMove(object sender, MouseButtonEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                var point = e.GetPosition(this);
-                App.ContinueSending(point);
-            }
-        }
-
+        
+        // Server and Client management methods
         private void ConnectButton_Click(object sender, RoutedEventArgs e)
         {
-            App.Connect(ipaddr.Text, int.Parse(port.Text));
+            if (client.Connect(ipaddr.Text, int.Parse(port.Text)) != 0) return;
 
+            canvas.IsEnabled = true;
             connectButton.IsEnabled = false;
             disconnectButton.IsEnabled = true;
         }
 
         private void DisconnectButton_Click(object sender, RoutedEventArgs e)
         {
-            App.Disconnect();
+            client.Disconnect();
 
+            canvas.IsEnabled = false;
             connectButton.IsEnabled = true;
             disconnectButton.IsEnabled = false;
         }
 
         private void HostButton_Click(object sender, RoutedEventArgs e)
         {
+            if (server.Start(canvas, ipaddr.Text, int.Parse(port.Text)) != 0) return;
 
+            //connectButton.IsEnabled = false; // czy to potrzebne?
+            hostButton.IsEnabled = false;
+            stopHostingButton.IsEnabled = true;
         }
 
         private void StopHostingButton_Click(object sender, RoutedEventArgs e)
         {
+            server.Stop();
 
+            connectButton.IsEnabled = true;
+            hostButton.IsEnabled = true;
+            stopHostingButton.IsEnabled = false;
+        }
+
+        // Canvas methods
+        private void Cp_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            if (cp.SelectedColor.HasValue) { currentColor = cp.SelectedColor.Value; };
+        }
+
+        public void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                currentPoint = e.GetPosition(this);
+                currentPolyline = new Polyline
+                {
+                    Stroke = new SolidColorBrush(currentColor),
+                    StrokeThickness = lineWidthSlider.Value,
+                    FillRule = FillRule.EvenOdd
+                };
+                canvas.Children.Add(currentPolyline);
+
+                client.StartSending(currentPoint);
+            }
+        }
+
+        public void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                currentPoint = e.GetPosition(this);
+                currentPolyline.Points.Add(currentPoint);
+
+                client.ContinueSending(currentPoint);
+            }
+        }
+
+        public void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            client.StopSending();
+        }
+
+        private void Canvas_MouseLeave(object sender, MouseEventArgs e) // to potrzebne? canvas i tak przytnie punkty do swojego obszaru...
+        {
+            //App.StopSending();
         }
     }
 }
